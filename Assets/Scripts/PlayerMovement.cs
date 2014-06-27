5b10e50dee58e44a2b8a1af76bbd12d021ce201d
WilsonKoder111 <WilsonKoder@Wilsons-MacBook-Pro.local>
2014-06-27 09:28:16 +0800

    Fixing bugs. [EXPERIMENTAL] Level Timer.
    List of changes/fixes:
    
    Added a level timer [EXPERIMENTAL]
    Made level harder
    Faster AI
    Fixed GUI Bugs not letting you restart
    Fixed scoring bug.
    
    ——————————————————————————
    
    www.youtube.com/wilsonkoder/

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,16 @@ public class PlayerMovement : MonoBehaviour {
 	public Transform Camera;
 	public Transform ProjectileTransform;
 	public GameObject Projectile;
-	public bool isPaused = false;
-	public bool hasWon = false;
-	public bool hasLost = false;
-	public bool canJump = false;
-	public bool canWin = false;
-	public int score;
+	public GameObject enemyObject;
+	private bool isPaused = false;
+	private bool hasWon = false;
+	private bool hasLost = false;
+	private bool canJump = false;
+	private bool canWin = false;
+	private bool isDead = false;
+	private int score;
+	private int enemyCount;
+	private string timeString;
 	//Old variables.
 	//public int enemyCount;
 	//public double wait;
@@ -23,6 +27,7 @@ public class PlayerMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//Initialization
+		isDead = false;
 		isPaused = false;
 		hasWon = false;
 		hasLost = false;
@@ -33,30 +38,49 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(enemyObject == null)
+		{
+			canWin = true;
+		}
+		timeString = Time.timeSinceLevelLoad.ToString();
 		//InvokeRepeating("Subtract", Time.deltaTime, 0.01f);
 		//waitString = wait.ToString();
 		//Player movement
 		//transform.Translate(Vector3.right * Input.GetAxis ("Horizontal") * playerSpeed * Time.deltaTime);
-		if(Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			hasWon = true;
+			canWin = true;
+		}
+
+		if(Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow))
+		{
 			rigidbody.velocity = new Vector3(-12, rigidbody.velocity.y, 0);
 		}
-		if(Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+		if(Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow))
+		{
 			rigidbody.velocity = new Vector3(12, rigidbody.velocity.y, 0);
 		}
 
 		//Jumping
 		//rigidbody.AddForce(Vector3.up * Input.GetAxis ("Jump") * 5 000f * Time.deltaTime);
-		if (Input.GetKeyDown(KeyCode.W)) {
-			if(canJump) {
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			if(canJump)
+			{
 				rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpSpeed, 0);
 			}
 		}
-		if (Input.GetKeyDown(KeyCode.S)) {
+		if (Input.GetKeyDown(KeyCode.S))
+		{
 			rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpSpeed * -1, 0);
 		}
 
 		//Shooting projectile
-		if(Input.GetKeyDown(KeyCode.Space)) {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
 			Instantiate(Projectile, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), ProjectileTransform.rotation);
 		}
 
@@ -65,52 +89,100 @@ public class PlayerMovement : MonoBehaviour {
 		Camera.position = new Vector3(transform.position.x, transform.position.y, Camera.position.z);
 
 		//Check if the players position is less than -3 on the y axis
-		if(transform.position.y < -3) {
+		if(transform.position.y < -3)
+		{
 			hasLost = true;
 		}
 
 		//Check to see if the user presses ESC to quit/pause the game.
-		if(Input.GetKeyDown (KeyCode.Escape)) {
+		if(Input.GetKeyDown (KeyCode.Escape))
+		{
 			Pause ();
 		}
 	}
 
 	void OnGUI() {
-		GUI.Box (new Rect(10, 10, 120, 40), "Platformer");
+		GUI.Box (new Rect(10, 10, 120, 60), "Platformer");
 		GUI.Label(new Rect(45, 25, 110, 20), "Score: " + score);
+		GUI.Label(new Rect(45, 45, 110, 20), timeString);
 		//GUI.Label(new Rect(45, 50, 220, 20), waitString);
 
 
-		if(isPaused) {
-			if (GUI.Button(new Rect(200, 200, 120, 40), "Quit")) {
+		if(isPaused)
+		{
+			if (GUI.Button(new Rect(200, 200, 120, 40), "Quit"))
+			{
 				Application.Quit ();
 			}
-			if (GUI.Button(new Rect(200, 250, 120, 40), "Unpause")) {
+			if (GUI.Button(new Rect(200, 250, 120, 40), "Unpause"))
+			{
 				Pause ();
 			}
 
 		}
-		if(hasWon) {
-			if(GUI.Button(new Rect(100, 100, 300, 100), "Quit Game?")) {
+		if(hasWon && canWin)
+		{
+			if(GUI.Button(new Rect(100, 100, 300, 100), "Quit Game?"))
+			{
 				Application.Quit();
 			}
-			if(GUI.Button(new Rect(100, 210, 300, 100), "Advance to next level?")) {
+			if(GUI.Button(new Rect(100, 210, 300, 100), "Advance to next level?"))
+			{
 				//Load next level.
 				//Application.LoadLevel(1);
 			}
-		}
-		if(hasLost) {
-			if(GUI.Button(new Rect(100, 100, 300, 100), "Quit Game?")) {
-				Application.Quit();
-			}
-			if(GUI.Button(new Rect(100, 210, 300, 100), "Retry?")) {
+			if(GUI.Button(new Rect(100, 320, 300, 100), "Retry?"))
+			{
 				isPaused = false;
 				hasWon = false;
 				hasLost = false;
 				score = 0;
 				canJump = false;
 				canWin = false;
+				timeString = "0";
+				
+				transform.position = new Vector3(-14, 2, 0);
+				//take one away from score
+			}
+		}
+		if(hasLost)
+		{
+			if(GUI.Button(new Rect(100, 100, 300, 100), "Quit Game?"))
+			{
+				Application.Quit();
+			}
+			if(GUI.Button(new Rect(100, 210, 300, 100), "Retry?"))
+			{
+				isPaused = false;
+				hasWon = false;
+				hasLost = false;
+				score = 0;
+				canJump = false;
+				canWin = false;
+				timeString = "0";
 
+				transform.position = new Vector3(-14, 2, 0);
+				//take one away from score
+			}
+		}
+		if(isDead)
+		{
+			if(GUI.Button(new Rect(100, 100, 300, 100), "Quit Game?"))
+			{
+				Application.Quit();
+			}
+			if(GUI.Button(new Rect(100, 210, 300, 100), "Retry?"))
+			{
+				isDead = false;
+				isPaused = false;
+				hasWon = false;
+				hasLost = false;
+				score = 0;
+				canJump = false;
+				canWin = false;
+				timeString = "0";
+				Time.timeScale = 1;
+				
 				transform.position = new Vector3(-14, 2, 0);
 				//take one away from score
 			}
@@ -119,38 +191,51 @@ public class PlayerMovement : MonoBehaviour {
 
 	//Pause function
 	void Pause() {
-		if(isPaused == true && !hasWon) {
+		if(isPaused == true && !hasWon)
+		{
 			Time.timeScale = 1;
 			Debug.Log ("Game unpaused");
 			isPaused = false;
-		} else if(isPaused == false && !hasWon) {
+		} 
+		else if(isPaused == false && !hasWon)
+		{
 			Time.timeScale = 0;
 			Debug.Log("Game paused");
 			isPaused = true;
 		}
-		if(hasWon) {
+		if(hasWon)
+		{
 			Time.timeScale = 0;
 			Debug.Log("Has won, therefore pausing");
 		}
 	}
 
-	void OnTriggerEnter() {
-		Debug.Log ("You Win!");
-		hasWon = true;
-		Pause();
-		score = score + 1;
-	}
-
-	void OnCollisionEnter(Collision collisionInfo) {
-		if(collisionInfo.transform.tag == "Enemy") {
-			Destroy(gameObject);
-			hasLost = true;
-		} else {
-		canJump = true;
+	void OnTriggerEnter()
+	{
+		if(canWin)
+		{
+			Debug.Log ("You Win!");
+			hasWon = true;
+			Pause();
+			score = score + 1;
 		}
 	}
 
-	void OnCollisionExit(Collision collisionInfo) {
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		if(collisionInfo.gameObject.tag == "Enemy")
+		{
+			hasLost = true;
+			Time.timeScale = 0;
+		} 
+		else
+		{
+			canJump = true;
+		}
+	}
+
+	void OnCollisionExit(Collision collisionInfo)
+	{
 		canJump = false;
 	}
 
---
+++ README.md
@@ -0,0 +1,7 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1265\cocoasubrtf200
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
+
+\f0\fs24 \cf0 You can play the game by download the repo as a ZIP, using the button on the side, or you can clone it to your desktop, all builds except for the MAC one are named Platform_Build. Replace platform with whatever platform you are on i.e. Linux. You can do whatever you want with this game, but you cannot take credit for it. That\'92s a dick move.}
